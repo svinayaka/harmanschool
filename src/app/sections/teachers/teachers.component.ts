5dac3d1bb4b2f080b614df31dc9f97146f574c9b
siddhi vinayaka <svinayaka290489@gmail.com>
2020-02-19 23:13:46 +0530

    update sections

--- src/app/sections/teachers/teachers.component.ts
+++ src/app/sections/teachers/teachers.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teachers.component.scss']
 })
 export class TeachersComponent implements OnInit {
-
+  // tslint:disable-next-line:max-line-length
+  txt = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.';
   constructor() { }
 
   ngOnInit(): void {
